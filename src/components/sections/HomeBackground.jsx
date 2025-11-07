c8340e86fcb9a08f512c03ca8693787aa32d7f1e
Samuel Luz Santana <slsamuelluz@gmail.com>
2025-11-06 21:32:49 -0300

    Implement code changes to enhance functionality and improve performance

--- src/components/sections/HomeBackground.jsx
+++ src/components/sections/HomeBackground.jsx
@@ -4,21 +4,20 @@ import maoDireita from "../../assets/elementos/mao-direita.png";
 const HomeBackground = () => {
   return (
     <>
-      {/* Mão Esquerda - Canto Inferior Esquerdo */}
-      <div className="pointer-events-none absolute bottom-[-8em] left-0 -z-10 md:bottom-[1em]">
-        <img
-          src={maoEsquerda}
-          alt="Mão esquerda"
-          className="h-[32em] w-auto object-contain sm:h-40 md:h-[28em] lg:h-56 xl:h-64"
-        />
-      </div>
-
       {/* Mão Direita - Canto Superior Direito */}
-      <div className="pointer-events-none absolute right-0 top-[-5rem] -z-10 md:top-[1rem]">
+      <div className="pointer-events-none absolute right-0 top-[-1rem] -z-10 md:top-[-1rem] lg:top-[3rem]">
         <img
           src={maoDireita}
           alt="Mão direita"
-          className="h-[32em] w-auto object-contain sm:h-40 md:h-[28em] lg:h-56 xl:h-64"
+          className="h-[25em] w-auto object-contain sm:h-40 md:h-[28em] lg:h-[32em] xl:h-[36em]"
+        />
+      </div>
+      {/* Mão Esquerda - Canto Inferior Esquerdo */}
+      <div className="pointer-events-none absolute bottom-[-3em] left-[-10em] -z-10 md:bottom-0">
+        <img
+          src={maoEsquerda}
+          alt="Mão esquerda"
+          className="h-[25em] w-auto object-contain sm:h-40 md:h-[28em] lg:h-[32em] xl:h-[36em]"
         />
       </div>
     </>
